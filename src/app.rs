7aba96f73c3eede4ad6088a640e2086d2c06c03c
Malte <malte.sparenborg@protonmail.com>
2025-02-02 10:51:49 +0100

    make slider fill full width and transform image for 0..1.0

--- src/app.rs
+++ src/app.rs
@@ -90,34 +90,6 @@ impl TemplateApp {
             ..Default::default()
         }
     }
-
-    // pub fn set_image(&mut self, ctx: &egui::Context) {
-    //     let image = image::ImageReader::open("assets/example_image.png")
-    //         .unwrap()
-    //         .decode()
-    //         .unwrap();
-
-    //     // let grayscale = image.grayscale().to_luma8();
-    //     // let grayscale_thresh = threshold(
-    //     //     &grayscale,
-    //     //     self.threshold.try_into().unwrap(),
-    //     //     imageproc::contrast::ThresholdType::Binary,
-    //     // );
-
-    //     // let rgba_image = DynamicImage::ImageLuma8(grayscale_thresh).to_rgba8();
-    //     // let size = [image.width() as _, image.height() as _];
-    //     // let pixels: &[u8] = image.as_flat_samples_u8().unwrap().samples;
-
-    //     // let texture_handle = egui::Context::load_texture(
-    //     //     ctx,
-    //     //     "selected_image_texture",
-    //     //     egui::ColorImage::from_rgba_unmultiplied(size, pixels),
-    //     //     Default::default(),
-    //     // );
-
-    //     // self.selected_texture_handle = Some(texture_handle);
-    //     log::info!("Loaded texture");
-    // }
 }
 
 impl eframe::App for TemplateApp {
@@ -145,73 +117,79 @@ impl eframe::App for TemplateApp {
         });
 
         // create floating window
-        egui::Window::new("floating_window").show(ctx, |ui| {
-            ui.heading("Options");
+        egui::SidePanel::new(egui::panel::Side::Left, "sidebar")
+            .resizable(false)
+            .show(ctx, |ui| {
+                ui.heading("Options");
 
-            TableBuilder::new(ui)
-                .column(Column::initial(100.0))
-                .column(Column::remainder())
-                .header(20.0, |mut header| {
-                    header.col(|ui| {
-                        ui.heading("Name");
-                    });
-                    header.col(|ui| {
-                        ui.heading("Value");
-                    });
-                })
-                .body(|mut body| {
-                    body.row(30.0, |mut row| {
-                        row.col(|ui| {
-                            ui.label("Threshold");
+                TableBuilder::new(ui)
+                    .column(Column::auto())
+                    .column(Column::auto())
+                    .header(20.0, |mut header| {
+                        header.col(|ui| {
+                            ui.heading("Name");
                         });
-                        row.col(|ui| {
-                            let response = ui.add(egui::Slider::new(&mut self.threshold, 0..=255));
+                        header.col(|ui| {
+                            ui.heading("Value");
+                        });
+                    })
+                    .body(|mut body| {
+                        body.row(30.0, |mut row| {
+                            row.col(|ui| {
+                                ui.label("Threshold");
+                            });
+                            row.col(|ui| {
+                                ui.style_mut().spacing.slider_width = 250.0;
 
-                            if response.changed() {
-                                // [TODO]: change image threshold
-                                let image = image::open("assets/example_image.png").unwrap();
-
-                                let grayscale = image.grayscale().to_luma8();
-                                let grayscale_thresh = threshold(
-                                    &grayscale,
-                                    self.threshold.try_into().unwrap(),
-                                    imageproc::contrast::ThresholdType::Binary,
+                                let response = ui.add(
+                                    egui::Slider::new(&mut self.threshold, 0..=255).step_by(1.0),
                                 );
 
-                                let texture_handle =
-                                    Some(load_texture(ctx, &grayscale_thresh.into()));
-                                self.selected_texture_handle = texture_handle;
+                                if response.changed() {
+                                    let image = image::open("assets/example_image.png").unwrap();
 
-                                log::info!("Threshold changed to {}", self.threshold);
-                            }
+                                    let grayscale = image.grayscale().to_luma8();
+                                    let grayscale_thresh = threshold(
+                                        &grayscale,
+                                        self.threshold.try_into().unwrap(),
+                                        imageproc::contrast::ThresholdType::Binary,
+                                    );
+
+                                    let texture_handle =
+                                        Some(load_texture(ctx, &grayscale_thresh.into()));
+                                    self.selected_texture_handle = texture_handle;
+
+                                    log::info!("Threshold changed to {}", self.threshold);
+                                }
+                            });
+                        });
+                        body.row(30.0, |mut row| {
+                            row.col(|ui| {
+                                ui.label("Minimal Pore Size");
+                            });
+                            row.col(|ui| {
+                                ui.style_mut().spacing.slider_width = 250.0;
+                                ui.add(egui::Slider::new(&mut self.minimal_pore_size, 0..=250));
+                            });
                         });
                     });
-                    body.row(30.0, |mut row| {
-                        row.col(|ui| {
-                            ui.label("Minimal Pore Size");
-                        });
-                        row.col(|ui| {
-                            ui.add(egui::Slider::new(&mut self.minimal_pore_size, 0..=250));
-                        });
-                    });
-                });
 
-            if ui.button("Select Region").clicked() {
-                log::info!("Select Region");
-            }
+                if ui.button("Select Region").clicked() {
+                    log::info!("Select Region");
+                }
 
-            if ui.button("Apply to Batch").clicked() {
-                log::info!("Apply to Batch");
-            }
+                if ui.button("Apply to Batch").clicked() {
+                    log::info!("Apply to Batch");
+                }
 
-            if ui.button("Download Results").clicked() {
-                log::info!("Download Results");
-            }
+                if ui.button("Download Results").clicked() {
+                    log::info!("Download Results");
+                }
 
-            ui.separator();
+                ui.separator();
 
-            ui.heading("Image List");
-        });
+                ui.heading("Image List");
+            });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             let plot_response = egui_plot::Plot::new("plot")
@@ -226,8 +204,8 @@ impl eframe::App for TemplateApp {
                     if let Some(handle) = &self.selected_texture_handle {
                         plot_ui.add(PlotImage::new(
                             handle.id(),
-                            PlotPoint::new(0.0, 0.0),
-                            Vec2::new(handle.aspect_ratio(), 1.0),
+                            PlotPoint::new(0.5, 1.0 / handle.aspect_ratio() / 2.0),
+                            Vec2::new(1.0, 1.0 / handle.aspect_ratio()),
                         ));
                     }
 
@@ -280,8 +258,6 @@ impl eframe::App for TemplateApp {
                     if plot_response.response.drag_stopped() {
                         let start = plot_response.transform.value_from_position(start);
                         let end = plot_response.transform.value_from_position(end);
-
-                        log::info!("Selected region: {:?} - {:?}", start, end);
 
                         self.region_rect_start = Some(start);
                         self.region_rect_end = Some(end);
