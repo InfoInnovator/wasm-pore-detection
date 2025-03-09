47d7732aad5c3a3b08cf560a8bf6e582f96f550c
Malte <malte.sparenborg@protonmail.com>
2025-03-09 16:30:04 +0100

    add zoom shortcut description in corresponding window

--- src/view/shortcut_window.rs
+++ src/view/shortcut_window.rs
@@ -10,6 +10,8 @@ pub fn display_shortcut_window(ctx: &egui::Context, app: &mut PoreDetectionApp) 
                 ui.label("Right Arrow: Next image");
                 ui.label("Enter: Apply region from previous image to current image");
                 ui.label("Drag right mouse button: Select region");
+                ui.label("Scroll wheel: Zoom in/out");
+                ui.label("Double click left mouse button: Reset zoom and center image");
             });
     }
 }
