ca360079019815cc2553c5f4df5c2b730b0f2c20
Malte <malte.sparenborg@protonmail.com>
2025-03-14 12:29:00 +0100

    update shortcut description for region selector

--- src/view/shortcut_window.rs
+++ src/view/shortcut_window.rs
@@ -9,7 +9,7 @@ pub fn display_shortcut_window(ctx: &egui::Context, app: &mut PoreDetectionApp) 
                 ui.label("Left Arrow: Previous image");
                 ui.label("Right Arrow: Next image");
                 ui.label("Enter: Apply region from previous image to current image");
-                ui.label("Drag right mouse button: Select region");
+                ui.label("Drag right mouse button: Select region (needs to be from top-left to bottom-right!)");
                 ui.label("Scroll wheel: Zoom in/out");
                 ui.label("Double click left mouse button: Reset zoom and center image");
             });
